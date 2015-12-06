9c1d69c920b07625838eebdac84cd75096050c4e
Holger Woltersdorf <hw@hollo.me>
2015-12-06 16:07:44 +0100

    Added base exception

---
+++ src/Exceptions/RepoTemplateException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @author hollodotme
+ */
+
+namespace hollodotme\RepoTemplate\Exceptions;
+
+/**
+ * Class RepoTemplateException
+ *
+ * @package hollodotme\RepoTemplate\Exceptions
+ */
+class RepoTemplateException extends \Exception
+{
+
+}
