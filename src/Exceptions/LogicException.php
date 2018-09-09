65ec08329e6b22cedfb2877a5a1a73259f9e9760
Holger Woltersdorf <hw@hollo.me>
2018-09-09 10:34:46 +0200

    Update exceptions

--- src/Exceptions/LogicException.php
+++ src/Exceptions/LogicException.php
@@ -1,14 +1,7 @@
 <?php declare(strict_types=1);
-/**
- * @author hollodotme
- */
 
 namespace VendorName\ProjectName\Exceptions;
 
-/**
- * Class RepoTemplateException
- * @package VendorName\ProjectName\Exceptions
- */
 class LogicException extends \LogicException
 {
 
--- src/Exceptions/RuntimeException.php
+++ src/Exceptions/RuntimeException.php
@@ -1,14 +1,7 @@
 <?php declare(strict_types=1);
-/**
- * @author hollodotme
- */
 
 namespace VendorName\ProjectName\Exceptions;
 
-/**
- * Class RuntimeException
- * @package VendorName\ProjectName\Exceptions
- */
 class RuntimeException extends \RuntimeException
 {
 
